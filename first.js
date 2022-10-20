b33b94e209640b4fb26e3789dee6f2640f9ee7d5
*sowrub <nazmulhasun7@gmail.com>
2022-10-20 13:15:50 +0600

    Allah

---
+++ first.js
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+console.log(33);
+=======
+console.log(33);
+>>>>>>> 0b7612c359d745512e6e443b4cea5eb34b289c2f
+//debugger
---
+++ second.js
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+console.log('55');
+=======
+console.log('55');
+>>>>>>> 0b7612c359d745512e6e443b4cea5eb34b289c2f
+//debugger
